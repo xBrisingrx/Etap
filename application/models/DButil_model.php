ef529a82daeb1bee698c03f6b48c138fd9fbf44b
Mauro Almiron <mdavid.almiron@gmail.com>
2022-03-01 00:38:50 -0300

    DButil cambios en fn get

--- application/models/DButil_model.php
+++ application/models/DButil_model.php
@@ -6,11 +6,11 @@ class DButil_model extends CI_Model {
     parent::__construct();
   }
 
-  function get( $table, $filtros ) {
-  	if($attr != null and $valor != null) {
-  		return $this->db->get_where($this->table, array( $filtros ))->result();
+  function get( $table, $filtros = null) {
+  	if($filtros != null) {
+  		return $this->db->get_where($table, $filtros )->result();
   	} else {
-    	 return $this->db->get($this->table)->result();
+    	 return $this->db->get($table)->result();
     }
   }
 
@@ -25,6 +25,26 @@ class DButil_model extends CI_Model {
   public function update_entry($table, $id, $entry) {
     $this->db->where('id', $id);
     return $this->db->update($table, $entry);
+  }
+
+  function destroy_entry($table, $id) {
+    $entry = $this->db->get_where($table, array('id'=>$id))->row();
+    $entry->activo = false;
+    $entry->updated_at = date('Y-m-d H:i:s');
+    $entry->user_last_updated_id = $this->session->userdata('id');
+    $this->db->where('id', $id);
+    return $this->db->update($table, $entry);
+  }
+
+  function existe($table, $col, $value, $id = null){
+    $this->db->select("id, $col")
+                ->from($table)
+                  ->where( $col, $value );
+    if ($id != null) {
+      $this->db->where('id !=', $id);
+    }
+    $query = $this->db->get();
+    return ( $query->num_rows() > 0 );
   }
 
 }
