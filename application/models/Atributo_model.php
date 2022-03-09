1fffc0daea9ef355d6673072c5d34a68f7600d14
Mauro Almiron <mdavid.almiron@gmail.com>
2022-03-09 16:41:34 -0300

    Metodo para informes

--- application/models/Atributo_model.php
+++ application/models/Atributo_model.php
@@ -21,6 +21,25 @@ class Atributo_model extends CI_Model {
     return $this->db->get()->result();
   }
 
+  function get_nombre_id( $tipo, $ids = null ) {
+    $atributo_ids = $ids;
+
+    $this->db->select('id, nombre')
+               ->from($this->table)
+                 ->where('activo', TRUE)
+                 ->where('tipo', $tipo);
+
+    if ( $ids != null ) {
+      $this->db->where('id = ', $ids[0]);
+      for ($i=1; $i < count($ids) ; $i++) { 
+        $this->db->or_where('id = ', $ids[$i]);
+      }
+    }
+
+    $this->db->order_by('nombre', 'asc');
+    return $this->db->get()->result();
+  }
+
   function insert_entry($attribute) {
     return $this->db->insert('atributos', $attribute);
   }
