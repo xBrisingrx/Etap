5e775a5857fe4192915b83012c7060a9370bc12d
Mauro Almiron <mdavid.almiron@gmail.com>
2022-03-10 01:07:58 -0300

    cargar modelos de ayuda

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array('DButil_model' => 'DButil', 'Fileutil_model' => 'Fileutil');
